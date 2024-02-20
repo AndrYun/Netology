6e17906dc0124da4729e57af79adce6aac6d0254
AndrYun <yunyatkin95@gmail.com>
2024-02-21 01:27:42 +0300

    fixes

--- eventsAndState/portfolioFilter/src/Toolbar.jsx
+++ eventsAndState/portfolioFilter/src/Toolbar.jsx
@@ -1,17 +1,13 @@
 function Toolbar({ filters, selected, onSelectFilter }) {
-    return (
-        <>
-            {filters.map((filter) => (
-                <button
-                key={filter}
-                className={`button btn-${filter.toLowerCase()}`}
-                onClick={onSelectFilter}
-                >
-                {filter}
-                </button>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {filters.map((filter) => (
+        <button key={filter} onClick={onSelectFilter}>
+          {filter}
+        </button>
+      ))}
+    </>
+  );
 }
 
-export default Toolbar
+export default Toolbar;
